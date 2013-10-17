0a4d57d27633b64233bb8d5c239a0e99e9c0166b
bhaity <bhaity@gmail.com>
2013-10-17 14:31:37 -0400

    basic form

--- mott.rb
+++ mott.rb
@@ -1,4 +1,21 @@
 require 'sinatra'
+require 'pony'
+
+Pony.options = {
+  :to => 'bhaity@gmail.com',
+  :from => 'village.green.west.mailer@gmail.com',
+  :headers => { 'Content-Type' => 'text/html' },
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => 'village.green.west.mailer@gmail.com',
+    :password             => '245w14st',
+    :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+  }
+}
 
 get '/' do 
 	erb :splash
@@ -6,4 +23,9 @@ end
 
 get '/contact' do
 	erb :contact
+end
+
+post '/mail_to' do
+  Pony.mail :subject => "Mott St. | New Enquiry Received",
+            :body    => erb(:email)
 end
