cf395d5ec5d1365ec0988f303b7c1c73f6f3ea62
Sikandar Shukla <bhaity@gmail.com>
2013-11-07 12:02:15 -0500

    use real email address for mailer, include bcc

--- mott.rb
+++ mott.rb
@@ -2,20 +2,20 @@ require 'sinatra'
 require 'pony'
 
 Pony.options = {
-  :to => 'bhaity@gmail.com',
-  :from => 'village.green.west.mailer@gmail.com',
+  :from => 'info@199mott.com',
   :headers => { 'Content-Type' => 'text/html' },
   :via => :smtp,
   :via_options => {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
     :enable_starttls_auto => true,
-    :user_name            => 'village.green.west.mailer@gmail.com',
-    :password             => '245w14st',
+    :user_name            => 'info@199mott.com',
+    :password             => '199mottst',
     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
   }
 }
+
 
 get '/' do 
 	erb :splash
@@ -27,5 +27,7 @@ end
 
 post '/mail_to' do
   Pony.mail :subject => "Mott St. | New Enquiry Received",
-            :body    => erb(:email)
+            :body    => erb(:email),
+            :to => params["Email Address"],
+            :bcc => 'info@199mott.com'
 end
